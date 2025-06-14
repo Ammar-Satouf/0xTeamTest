ed1635a879f7561ad601353de2aae9b17940ac69
Ammar-Satouf <viberus512@gmail.com>
2025-06-15 00:12:44 +0300

    Create resources.py

---
+++ resources.py
@@ -0,0 +1,92 @@
+resources = {
+    "1": {
+        "2": {
+            "theoretical": {
+                "جبر خطي": {
+                    "gate": [0,],
+                    "komit": [5, 7, 8, 9, 10],
+                    "student_written": [43, 44, 45, 46, 47, 48, 49],
+                    "summaries": [72, 73, 74, 75, 76],
+                    "exams": [0],
+                    "notes": [0],
+                },
+                "فيزياء انصاف نواقل": {
+                    "gate": [0],
+                    "komit": [45, 46, 47],
+                    "student_written": [0],
+                    "summaries": [11, 12],
+                    "exams": [0],
+                    "notes": [37],
+                },
+                "برمجة 2": {
+                    "gate": [22, 23, 24, 25, 26],
+                    "komit": [17, 18, 19, 20, 21, 22],
+                    "student_written": [58, 59, 60, 61, 62, 63, 64],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [24, 25, 26, 27, 28],
+                },
+                "تحليل 2": {
+                    "gate": [0],
+                    "komit": [30, 31, 32, 33, 34, 35],
+                    "student_written": [66, 67, 68, 69, 70],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [0],
+                },
+                "لغة انجليزية 2": {
+                    "gate": [0],
+                    "komit": [49],
+                    "student_written": [0],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [0],
+                },
+            },
+            "practical": {
+                "جبر خطي": {
+                    "gate": [30, 31],
+                    "komit": [12, 13, 14, 15],
+                    "student_written": [0],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [0],
+                },
+                "فيزياء انصاف نواقل": {
+                    "gate": [0],
+                    "komit": [45, 46, 47],
+                    "student_written": [51, 52, 53, 54, 55, 56],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [0],
+                },
+                "برمجة 2": {
+                    "gate": [28],
+                    "komit": [24, 25, 26, 27, 28],
+                    "student_written": [0],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [30, 31, 32, 33, 34, 35],
+                },
+                "تحليل 2": {
+                    "gate": [0],
+                    "komit": [37, 38, 39, 40, 41, 42, 43],
+                    "student_written": [0],
+                    "summaries": [0],
+                    "exams": [0],
+                    "notes": [0],
+                },
+            }
+        }
+    }
+}
+
+channel_ids = {
+    "gate": -1002739707573,
+    "komit": -1002883141998,
+    "student_written": -1002701154329,
+    "summaries": -1002890234972,
+    "exams": -1002449310532,
+    "notes": -1002834270732,
+}
+temporary_culture_doc = 51
